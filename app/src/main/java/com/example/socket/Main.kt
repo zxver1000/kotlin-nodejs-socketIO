80a9ddf8c4d6e12ba3c4ac79243eb80d3e44830f
zxver1000 <wlwhs5014@naver.com>
2022-05-10 02:26:07 +0900

    1

--- app/src/main/java/com/example/socket/Main.kt
+++ app/src/main/java/com/example/socket/Main.kt
@@ -1,46 +1,63 @@
 package com.example.socket
 
+import android.app.Fragment
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.FrameLayout
+
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import main_fragment.*
 
 class Main : AppCompatActivity() {
+
+   private val fl: FrameLayout by lazy{
+       findViewById(R.id.fl_container)
+   }
+    private val bn: BottomNavigationView by lazy{
+        findViewById(R.id.bnv_main)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main2)
-        // 하단 탭이 눌렸을 때 화면을 전환하기 위해선 이벤트 처리하기 위해 BottomNavigationView 객체 생성
-        var bnv_main = findViewById(R.id.bnv_main) as BottomNavigationView
 
-        // OnNavigationItemSelectedListener를 통해 탭 아이템 선택 시 이벤트를 처리
-        // navi_menu.xml 에서 설정했던 각 아이템들의 id를 통해 알맞은 프래그먼트로 변경하게 한다.
-        bnv_main.run { setOnNavigationItemSelectedListener {
-            when(it.itemId) {
-                R.id.first -> {
-                    // 다른 프래그먼트 화면으로 이동하는 기능
-                    val homeFragment = homeFragment()
-                    supportFragmentManager.beginTransaction().replace(R.id.fl_container, homeFragment).commit()
-                }
-                R.id.second -> {
-                    val boardFragment = boardFragment()
-                    supportFragmentManager.beginTransaction().replace(R.id.fl_container, boardFragment).commit()
-                }
-                R.id.third -> {
-                    val badgeFragment = badgeFragment()
-                    supportFragmentManager.beginTransaction().replace(R.id.fl_container, badgeFragment).commit()
-                }
-                R.id.fourth -> {
-                    val youtubeFragment = youtubeFragment()
-                    supportFragmentManager.beginTransaction().replace(R.id.fl_container, youtubeFragment).commit()
-                }
-                R.id.fifth -> {
-                    val cameraFragment = cameraFragment()
-                    supportFragmentManager.beginTransaction().replace(R.id.fl_container, cameraFragment).commit()
-                }
-            }
-            true
-        }
-            selectedItemId = R.id.first
-        }
+
+        setContentView(R.layout.activity_main2)
+        supportFragmentManager.beginTransaction().add(fl.id,homeFragment()).commit()
+   bn.setOnNavigationItemSelectedListener { item ->
+       when(item.itemId){
+           R.id.first->{
+               supportFragmentManager.beginTransaction().replace(fl.id,homeFragment()).commit()
+               true
+           }
+           R.id.second->{
+               supportFragmentManager.beginTransaction().replace(fl.id,badgeFragment()).commit()
+               true
+           }
+           R.id.third->{
+               supportFragmentManager.beginTransaction().replace(fl.id,youtubeFragment()).commit()
+               true
+
+           }
+           R.id.fourth->{
+               supportFragmentManager.beginTransaction().replace(fl.id,boardFragment()).commit()
+               true
+           }
+
+           else -> {
+               supportFragmentManager.beginTransaction().replace(fl.id, cameraFragment()).commit()
+           true
+           }
+
+           }
+
+
+   }
+init()
+
+
     }
+    fun init(){
+
+    }
+
 }
+
--- app/src/main/java/main_fragment/cameraFragment.kt
+++ app/src/main/java/main_fragment/cameraFragment.kt
@@ -1,14 +1,22 @@
 package main_fragment
 
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.FrameLayout
+import androidx.annotation.RequiresApi
+
 import com.example.socket.R
 
-import com.example.socket.databinding.FragmentCameraBinding
+
+import com.google.firebase.ml.vision.face.FirebaseVisionFaceDetectorOptions
+
 import io.socket.client.IO
 import io.socket.client.Socket
 import io.socket.emitter.Emitter
@@ -18,10 +26,12 @@ import java.net.URISyntaxException
 class cameraFragment : Fragment() {
 
     lateinit var mSocket: Socket
-    lateinit var binding: FragmentCameraBinding
+
+
     val onConnect = Emitter.Listener {
         mSocket.emit("connectReceive", "hihi")
     }
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -29,7 +39,7 @@ class cameraFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
 
-        binding= FragmentCameraBinding.inflate(layoutInflater,container,false)
+
         try {
 
             mSocket = IO.socket("http://192.168.56.1:8080")
@@ -42,20 +52,24 @@ class cameraFragment : Fragment() {
         }
         mSocket.on(Socket.EVENT_CONNECT, onConnect)
         init()
-        return binding!!.root
+        return inflater.inflate(R.layout.fragment_camera, container, false)
     }
 
 
     fun init(){
-        binding.sendbutton.setOnClickListener{
-            var msg=binding.edittext.text.toString()
-            var s1=1
+         val send_button:Button=requireView().findViewById(R.id.sendbutton)
+
+        send_button.setOnClickListener{
+            val s1:EditText=requireView().findViewById(R.id.edittext)
+            var msg=s1.text.toString()
+            var s2=1
             val s=object{ val 감자="1"
                 val 나이="2"}
             mSocket.emit("connectReceive",msg)
             mSocket.emit("connectReceive",s1)
 
         }
+
     }
 
 }
